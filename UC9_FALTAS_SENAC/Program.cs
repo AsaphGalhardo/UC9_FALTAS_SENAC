cc45112ca2ef93ad918c60ad352450f8231d6cd0
AsaphGalhardo <asaphgalhardo@gmail.com>
2023-03-07 21:19:57 -0300

    Modificação

--- UC9_FALTAS_SENAC/Program.cs
+++ UC9_FALTAS_SENAC/Program.cs
@@ -1,1 +1,21 @@
 ﻿string[,] faltas = new string[2, 3];
+string opccao;
+int l, c;
+l= 0;
+c= 0;
+
+for (l = 0; l <= 2; l++)
+{
+    Console.WriteLine("Informe o numero da UC");
+    faltas[l, c++] = Console.ReadLine();
+    Console.WriteLine("Informe a descrição da  UC");
+    faltas[l, c++] = Console.ReadLine();
+    Console.WriteLine("Informe a carga horaria da  UC");
+    faltas[l, c++] = Console.ReadLine();
+    c = 0;
+}
+
+ for ()
+{
+
+}
